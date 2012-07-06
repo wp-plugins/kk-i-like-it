bb40b92f06fdf45836fbeadd550c95aada737fcc
Krzy-siek <Krzy-siek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-06 12:36:51 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kk-i-like-it/trunk@568254 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- documentation.php
+++ documentation.php
@@ -16,8 +16,28 @@ global $kkplugin, $wersja_plugin, $wpdb;
 		<div class="kkadmin-text">
 			<div class="kkadmin-text-wew">
 				
+				<h2>1. Jak dodać przycisk w dowolne miejsce wewnątrz pętli WP LOOP?</h2>
+				<p>Otwieramy plik, w którym chcemy dodać przycisk (np. index.php). Wewnątrz pętli WP LOOP, w miejscu, gdzie powinien się wyświetlić przycisk dodajemy fragment kodu:</p>
+				<code>
+				if(function_exists(kkLikeButton())){<br />
+				kkLikeButton();<br />
+				}
+				</code>
+				<p>Dodatkowo możesz wyświetlić w dowolnym miescu pętli informacje o liczbie polubień danej treści. Możesz to uczynić dodając fragment kodu:</p>
+				<code>
+				if(function_exists(kkLikeRating())){<br />
+				kkLikeRating();<br />
+				}
+				</code>
+				<p></p>
 				
+				<h2>2. Jak dodać shortcode?</h2>
+				<p>W edytorze tekstu WYSIWYG (podczas edycji treści) wklej tag:</p>
 				
+				<code>[kklike_button]</code> - jeśli chcesz wyświetlić przycisk<br />
+				lub<br />
+				<code>[kklike_rating]</code> - jeśli chcesz wyświetkić informację o ilości polubień danej treści<br />
+				<p></p>
 			</div>
 		</div>
 		<div class="kkadmin-sidebar">
