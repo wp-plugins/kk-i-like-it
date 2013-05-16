1df79445b7f62b749ebed226b6e9f3ccd8b94aa6
Krzy-siek <Krzy-siek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-16 19:31:33 +0000

    * Dotacje
    git-svn-id: https://plugins.svn.wordpress.org/kk-i-like-it/trunk@714040 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,29 @@
 
 <div class="kkadmin-sidebar-box" style="font-size: 10px; line-height: 12px;">
+	<p style="margin-bottom: 0;">
+		This plugin is totally free.<br />
+		If you like it and would like to contribute to its development, please support my work by...<br />
+	</p>
+
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" id="donate-form">
+		<input type="hidden" name="cmd" value="_donations">
+		<input type="hidden" name="business" value="krzysztof.furtak@gmail.com">
+		<input type="hidden" name="lc" value="GB">
+		<input type="hidden" name="item_name" value="KKILikeIt">
+		<input type="hidden" name="no_note" value="0">
+		<input type="hidden" name="currency_code" value="USD">
+		<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest">
+		<input type="image" id="donate-button" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+		<img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1">
+	</form>
+
+
+	<a href="#" class="buy-coffe" onclick="javascript: jQuery('#donate-form').submit();">
+		<img src="<?php echo WP_PLUGIN_URL; ?>/kk-i-like-it/images/cup-coffee-icon.png" alt="" style="width: 30px; height: auto; vertical-align: middle;" /> <span style="vertical-align: middle;">Buying me a cup of coffee</span>
+	</a>
+
+</div>
+<div class="kkadmin-sidebar-box" style="font-size: 10px; line-height: 12px;">
 	<p>Would you like to be informed about updates?<br />Follow us and be always updated.</p>
 	<a href="https://twitter.com/KrzysiekFurtak" class="twitter-follow-button" data-show-count="true" data-show-screen-name="false">Follow @KrzysiekFurtak</a>
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
